e9cb7c1275130cc08cda015b57551917579734e6
Chris Guarino <chrisguarino@chriss-mbp.myfiosgateway.com>
2022-07-19 14:50:46 -0400

    Hangman2

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -3,24 +3,35 @@ import string
 from words import words 
 
 def get_valid_word(words): 
-    word = random.choice(words) 
-    while '-' or ' ' in words: 
+    word = random.choice(words) #Randomly chooses a word from the given list. 
+    while '-' in word or ' ' in word: 
         word = random.choice(words)
-    
-    return word 
+
+    return word.upper()
 
 def hangman(): 
     word = get_valid_word(words)
     word_letters = set(word) #cast word letters to a set. 
     alphabet = set(string.ascii_uppercase) #pre-existing library
     used_letters = set() #Already guessed letters
+    
+    while len(word_letters) > 0:
 
-    user_letter = input('Guess a letter: ').upper() 
-    if user_letter in alphabet - used_letters: 
-        used_letters.add(user_letter)
-        if user_letter in word_letters: 
-            word_letters.remove(user_letter)
+        print('You have used these letters: ', ' '.join(used_letters)) #Used Letters
+        word_list = [letter if letter in used_letters else '-' for letter in word]#Gives the user the word they are guessing
+        print('Current word: ', ' '.join(word_list))
 
+        user_letter = input('Guess a letter: ').upper() 
+        if user_letter in alphabet - used_letters: 
+            used_letters.add(user_letter)
+            if user_letter in word_letters: 
+                word_letters.remove(user_letter)
+        elif user_letter in used_letters: 
+            print('You can\'t use that letter again.')
+        else: 
+            print('Not the character we are looking for.')
 
-user_input = input("Type something...")
-print(user_input)
+# user_input = input("Type something...")
+# print(user_input)
+
+hangman()
