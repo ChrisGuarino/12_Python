7a8275bc76357150711dc3e75029fe3f2979549a
Chris Guarino <chrisguarino@chriss-mbp.myfiosgateway.com>
2022-07-20 08:45:46 -0400

    Hangman Done, GUI?

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -14,10 +14,11 @@ def hangman():
     word_letters = set(word) #cast word letters to a set. 
     alphabet = set(string.ascii_uppercase) #pre-existing library
     used_letters = set() #Already guessed letters
-    
-    while len(word_letters) > 0:
+    lives = 6
 
-        print('You have used these letters: ', ' '.join(used_letters)) #Used Letters
+    while len(word_letters) > 0 and lives > 0:
+
+        print('You have used these letters: ', ' '.join(used_letters), f'\nYou have {lives} lives left.') #Used Letters
         word_list = [letter if letter in used_letters else '-' for letter in word]#Gives the user the word they are guessing
         print('Current word: ', ' '.join(word_list))
 
@@ -26,12 +27,15 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters: 
                 word_letters.remove(user_letter)
+            else: 
+                lives -= 1 
+                print('Letter is not in the word.')
         elif user_letter in used_letters: 
             print('You can\'t use that letter again.')
         else: 
             print('Not the character we are looking for.')
-
-# user_input = input("Type something...")
-# print(user_input)
+    if lives == 0: 
+        print('You lost...bitch')
+    else: print('Nice work you guessed the word!')
 
 hangman()
